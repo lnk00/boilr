cf07c137382c00358fcb227fb5c8f98c2094d63f
Damien Dumontet <damien.dumontet@protonmail.com>
2023-04-06 18:49:46 -0300

    implement dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.container}>
+      <Nav />
       <div className={styles.hero}>
-        <Nav />
         <div className={styles.version}>Version 1.0.0</div>
         <h1 className={`${sora.className} ${styles.headline}`}>The best boilerplate for your next SaaS idea.</h1>
         <Btn>Discover your boilerplate</Btn>
